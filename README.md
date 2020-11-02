d38a6b6761097eb8231947004a464583517f926f
nakama <mingjiazhenyoutai@gmail.com>
2020-11-02 13:23:45 +0900

    deploy on Vercel, updete README.md

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 ### 参考サイト
 
-https://qiita.com/n0bisuke/items/ceaa09ef8898bee8369d
+- LINE Bot:
+  https://qiita.com/n0bisuke/items/ceaa09ef8898bee8369d
+- Vercel:
+  https://qiita.com/n0bisuke/items/a5f49193fa68f28a7f25
+  https://qiita.com/n0bisuke/items/901154531ea14f978bd4
 
 ### 立ち上げ方
+
+※Vercel でサーバにデプロイをした場合はこの作業は行う必要がない
 
 - サーバを立ち上げる
   `npm start`
@@ -13,3 +19,9 @@ https://qiita.com/n0bisuke/items/ceaa09ef8898bee8369d
 - トンネリングサーバーを起動すると表示される"https://"から始まるアドレスを下記サイトの Webhook 設定の"/webhook"の前に貼り付けて更新する
   (※この作業はトンネリングサーバーを立ち上げるたびに毎度行う必要がある)
   https://developers.line.biz/console/channel/1655190346/messaging-api
+
+### Vercel
+
+- プログラムを更新したら下記のコマンドでデプロイする
+- vercel deploy command
+  `vercel --prod`
--- api/server.js
+++ api/server.js
@@ -14,17 +14,17 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)")); //ブラウザ確認用(無くても問題ない)
 app.post("/webhook", line.middleware(config), (req, res) => {
-  console.log(req.body.events);
+  // console.log(req.body.events);
 
   //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
-  if (
-    req.body.events[0].replyToken === "00000000000000000000000000000000" &&
-    req.body.events[1].replyToken === "ffffffffffffffffffffffffffffffff"
-  ) {
-    res.send("Hello LINE BOT!(POST)");
-    console.log("疎通確認用");
-    return;
-  }
+  // if (
+  //   req.body.events[0].replyToken === "00000000000000000000000000000000" &&
+  //   req.body.events[1].replyToken === "ffffffffffffffffffffffffffffffff"
+  // ) {
+  //   res.send("Hello LINE BOT!(POST)");
+  //   console.log("疎通確認用");
+  //   return;
+  // }
 
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => {
@@ -39,15 +39,26 @@ app.post("/webhook", line.middleware(config), (req, res) => {
 const client = new line.Client(config);
 
 async function handleEvent(event) {
-  // console.log("event" + event);
+  console.log(event);
   if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
+
+  let replyText = "";
+  if (event.message.text === "こんにちは") {
+    replyText = "こんばんは！！";
+  } else {
+    replyText = "ウザwwwwwww";
+  }
+
   return client.replyMessage(event.replyToken, {
     type: "text",
-    text: event.message.text, //実際に返信の言葉を入れる箇所
+    text: replyText, //実際に返信の言葉を入れる箇所
   });
 }
 
-app.listen(PORT);
+// app.listen(PORT);
+// console.log(`Server running at ${PORT}`);
+
+process.env.NOW_REGION ? (module.exports = app) : app.listen(PORT);
 console.log(`Server running at ${PORT}`);

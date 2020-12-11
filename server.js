bc8279a7ba99d066621a4b6448ae8c516c5ef6ac
nakama <mingjiazhenyoutai@gmail.com>
2020-12-11 10:52:35 +0900

    heroku-4

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ const getDescriptionV2 = async (userId, word, option = "") => {
   console.log(escape(word));
   const page = await wiki.page(word);
   const summary = await page.summary();
-  let content = await page.content();
   console.log(summary);
+  let content = await page.content();
   console.log(`https://ja.wikipedia.org/wiki/${word}`);
   let titleList = [];
   let responseText = "";

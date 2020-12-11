90d86348cb279cc7cae741034ec4f4b513537a08
nakama <mingjiazhenyoutai@gmail.com>
2020-12-11 11:16:35 +0900

    heroku-test

--- server.js
+++ server.js
@@ -76,6 +76,7 @@ const getDescriptionV2 = async (userId, word, option = "") => {
   const page = await wiki.page(word);
   const summary = await page.summary();
   let content = await page.content();
+  console.log(content);
   // console.log(`https://ja.wikipedia.org/wiki/${word}`);
   let titleList = [];
   let responseText = "";

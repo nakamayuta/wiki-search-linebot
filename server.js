31c0e7b1bb18bb8839ddbd413ddd9987be5b8a3e
nakama <mingjiazhenyoutai@gmail.com>
2020-12-11 10:46:17 +0900

    heroku-4

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ const getDescriptionV2 = async (userId, word, option = "") => {
   console.log(word);
   console.log(escape(word));
   const page = await wiki.page(word);
-  console.log(wiki.page(escape(word)));
+  console.log(await wiki.page(escape(word)).summary());
   const summary = await page.summary();
   let content = await page.content();
   let titleList = [];
